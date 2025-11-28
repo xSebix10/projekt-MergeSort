ad4b9bde164db75f02bcd3ad2b2f703807e4b87a
Sebastian Tatara <seba.kontakt@wp.pl>
2025-11-28 21:58:31 +0100

    Create MergeSorter template class interface

---
+++ MergeSorter.h
@@ -0,0 +1,8 @@
+template <typename T>
+class MergeSorter {
+public:
+    void sort(std::vector<T>& arr) { /* TODO */ }
+private:
+    void mergeSortRecursive(std::vector<T>& arr, int left, int right) { }
+    void merge(std::vector<T>& arr, int left, int mid, int right) { }
+};
